3a67bea43d2e9efa12ef18ece830ed81261fd6ec
tsandrini <tomas.sandrini@seznam.cz>
2024-08-05 15:21:51 +0200

    refactor(files): purify drirc fetchGit

--- utilities/files.nix
+++ utilities/files.nix
@@ -1,8 +1,11 @@
 { lib, ... }:
 
 rec {
-    drirc = builtins.fetchGit {
-        url = "https://github.com/NicolasGuilloux/blade-shadow-beta";
-        ref = "master";
-      } + "/resources/drirc";
+  drirc =
+    builtins.fetchGit {
+      url = "https://github.com/NicolasGuilloux/blade-shadow-beta";
+      ref = "master";
+      rev = "a58bdba241262723431f65e736e4b6dab1f75454";
+    }
+    + "/resources/drirc";
 }
